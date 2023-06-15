041870e7a0e5890562d9cc71706d3a1ea07241c0
Changju Hwang <hchju777@korea.ac.kr>
2023-06-15 15:22:44 +0900

    [MAPF] Add Observer Interface

--- build/CMakeFiles/server.dir/DependInfo.cmake
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -24,8 +24,8 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
-  "/home/changju/multibot_ws/install/multibot_ros2_interface/include"
   "/opt/ros/foxy/include"
+  "/home/changju/multibot_ws/install/multibot_ros2_interface/include"
   )
 
 # Targets to which this target links.
